cb42e5906fd08db57e6440fb74107993f5216df7
avgupta <av1659@gmail.com>
2018-12-06 20:50:05 -0800

    fixed avg

--- baseline_debiaser.py
+++ baseline_debiaser.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-EPSILON = 0.001
+EPSILON = 0.000001
 
 def single_iteration(ratings_graph, biases, true_ratings, alpha):
 	# Update Ratings
@@ -14,25 +14,26 @@ def single_iteration(ratings_graph, biases, true_ratings, alpha):
 	# Update Biases
 	indiv_biases = ratings_graph.get_ratings() - next_true_ratings
 	bias_denoms = ratings_graph.get_user_rating_counts()
-	next_biases = np.sum(ratings_graph.get_graph() * indiv_biases, axis=1) / bias_denoms
+	next_biases = (1-alpha)*biases + alpha*(np.sum(ratings_graph.get_graph() * indiv_biases, axis=1) / bias_denoms)
 
 	converged = True
-	if (true_ratings is not None and np.any(np.abs(true_ratings - next_true_ratings) > EPSILON)) or \
+	if ((true_ratings is not None) and np.any(np.abs(true_ratings - next_true_ratings) > EPSILON)) or \
 		np.any(np.abs(biases - next_biases) > EPSILON):
 		converged = False
 
 	return converged, next_true_ratings, next_biases
-	
 
-def debias_ratings_baseline(ratings_graph, alpha, max_iters):
+
+def debias_ratings_baseline(ratings_graph, initial_alpha, decay_rate, max_iters):
 	num_users, num_entities = ratings_graph.get_graph_shape()
 	ground_truth_ratings = ratings_graph.get_ground_truth_ratings()
-	true_ratings = []
-	biases = [np.random.rand(num_users)]
+	true_ratings = [np.random.uniform((num_entities,))]
+	biases = [np.random.uniform(low = -1, high = 1, size = (num_users,))]
 	errors = []
 
 	converged = False
 	num_iters = 0
+	alpha = initial_alpha
 	while not converged and num_iters < max_iters:
 		true_rate_or_none = None if not true_ratings else true_ratings[-1]
 		iter_out = single_iteration(ratings_graph, biases[-1], true_rate_or_none, alpha)
@@ -44,5 +45,6 @@ def debias_ratings_baseline(ratings_graph, alpha, max_iters):
 			errors.append(np.sqrt(np.mean((next_true_ratings - ground_truth_ratings)**2)))
 
 		num_iters += 1
+		alpha = alpha/decay_rate
 
 	return biases, true_ratings, errors
